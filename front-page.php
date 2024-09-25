c7770f8eb0ef45342d8603939b140775fd43b61d
zekino <zekino.0613@gmail.com>
2024-09-26 01:48:01 +0900

    カスタム投稿作成【news】

--- front-page.php
+++ front-page.php
@@ -57,41 +57,93 @@
           <h2 class="section-title ">PRODUCT</h2>
           <a class="more-btn bb" href="<?php the_permalink(); ?>">MORE</a> 
         </div>
+
         <div class="product-postlist">            
-        <?php
-        // WP_Query を使ってカスタム投稿タイプ 'product' の最新６件の投稿を取得
-        $news_query = new WP_Query(array(
-            'post_type' => 'product',      // 取得する投稿タイプを 'news' に指定
-            'posts_per_page' => 6,      // 取得する投稿の数を 6 に指定
-            'order' => 'ASC' //昇順で並び替える
-        ));
+          <?php
+          // WP_Query を使ってカスタム投稿タイプ 'product' の最新６件の投稿を取得
+          $news_query = new WP_Query(array(
+              'post_type' => 'product',      // 取得する投稿タイプを 'news' に指定
+              'posts_per_page' => 6,      // 取得する投稿の数を 6 に指定
+              'order' => 'ASC' //昇順で並び替える
+          ));
 
         
-        // 投稿が存在するか確認
-        if ($news_query->have_posts()) :
-            // 投稿が存在する場合、ループを開始
-            while ($news_query->have_posts()) : $news_query->the_post(); //投稿が存在するか確認 : まだ投稿が残っているかチェック
-                ?>
-              
-              <div class="product-post">
-                <?php if (has_post_thumbnail()) : ?>
-                  <div class="post-thumbnail">
-                      <?php the_post_thumbnail('medium'); // サムネイル画像を 'medium' サイズで表示 ?>
-                  </div>
-                  <h3 class="product-title"><?php the_title(); ?></h3> <!-- 投稿のタイトルを表示 -->
-                <?php endif; ?>
-              </div>    
-        <?php
+          // 投稿が存在するか確認
+          if ($news_query->have_posts()) :
+              // 投稿が存在する場合、ループを開始
+              while ($news_query->have_posts()) : $news_query->the_post(); //投稿が存在するか確認 : まだ投稿が残っているかチェック
+                  ?>
+                
+                <div class="product-post">
+                  <?php if (has_post_thumbnail()) : ?>
+                    <div class="post-thumbnail">
+                        <?php the_post_thumbnail('medium'); // サムネイル画像を 'medium' サイズで表示 ?>
+                    </div>
+                    <h3 class="product-title"><?php the_title(); ?></h3> <!-- 投稿のタイトルを表示 -->
+                  <?php endif; ?>
+                </div>    
+          <?php
             endwhile;
             // クエリ後のグローバルな投稿データをリセット
-            wp_reset_postdata();
-        else :
+              wp_reset_postdata();
+            else :
             // 投稿が見つからない場合のメッセージを表示
             echo '<p>No news found.</p>';
-        endif;
-        ?>
-      </div><!-- /.product-postlist -->  
-      <a class="more-btn aa" href="<?php the_permalink(); ?>">MORE</a> <!-- 投稿の詳細ページへのリンクを表示 -->
+            endif;
+          ?>
+        </div><!-- /.product-postlist -->  
+        <a class="more-btn aa" href="<?php the_permalink(); ?>">MORE</a> <!-- 投稿の詳細ページへのリンクを表示 -->
+      </div>  
+    </section>
+
+
+
+    <section id ="news">
+      <div class="news__inner">
+        <div class="news__inner--flex">
+          <h2 class="section-title ">NEWS</h2>
+          <a class="more-btn bb" href="<?php the_permalink(); ?>">MORE</a> 
+        </div>
+
+        <div class="news-postlist">            
+          <?php
+          // WP_Query を使ってカスタム投稿タイプ 'news' の最新６件の投稿を取得
+          $news_query = new WP_Query(array(
+              'post_type' => 'news',      // 取得する投稿タイプを 'news' に指定
+              'posts_per_page' => 6,      // 取得する投稿の数を 6 に指定
+              'order' => 'DESC' //降順で並び替える
+          ));
+
+        
+          // 投稿が存在するか確認
+          if ($news_query->have_posts()) :
+              // 投稿が存在する場合、ループを開始
+              while ($news_query->have_posts()) : $news_query->the_post(); //投稿が存在するか確認 : まだ投稿が残っているかチェック
+                  ?>
+                
+                <div class="news-post">
+                  <?php if (has_post_thumbnail()) : ?>
+                    <div class="post-thumbnail">
+                        <?php the_post_thumbnail('medium'); ?> <!-- サムネイル画像を 'medium' サイズで表示 -->
+                    </div>
+                    <div class="post-textarea">
+                      <h3 class="news-title"><?php the_title(); ?></h3> <!-- 投稿のタイトルを表示 -->
+                      <time class="post-date"><?php echo get_the_date(); ?></time>
+                      <p><?php the_excerpt(); ?></p>  <!--投稿の抜粋を表示 -->
+                    </div><!-- /.post-textarea -->
+                  <?php endif; ?>
+                </div>    
+          <?php
+            endwhile;
+            // クエリ後のグローバルな投稿データをリセット
+              wp_reset_postdata();
+            else :
+            // 投稿が見つからない場合のメッセージを表示
+            echo '<p>No news found.</p>';
+            endif;
+          ?>
+        </div><!-- /.product-postlist -->  
+        <a class="more-btn aa" href="<?php the_permalink(); ?>">MORE</a> <!-- 投稿の詳細ページへのリンクを表示 -->
       </div>  
     </section>
 
